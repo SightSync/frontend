b67a3ce08682a0e40c4ea67d6d2f78e9167e8a23
Oriol Agost Batalla <gpg18@agost.info>
2023-12-02 23:31:35 +0100

    feat: add basic audio usage sample

--- app/src/main/java/com/example/sightsync/MainActivity.kt
+++ app/src/main/java/com/example/sightsync/MainActivity.kt
@@ -3,41 +3,92 @@ package com.example.sightsync
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.example.sightsync.playback.AndroidAudioPlayer
+import com.example.sightsync.recorder.AndroidAudioRecorder
 import com.example.sightsync.ui.theme.SightsyncTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
+    private val recorder by lazy {
+        AndroidAudioRecorder(applicationContext)
+    }
+    private val player by lazy {
+        AndroidAudioPlayer(applicationContext)
+    }
+    private var audioFile: File? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                android.Manifest.permission.RECORD_AUDIO,
+            ), 0
+        )
         setContent {
             SightsyncTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    val recordInteractionSource = remember { MutableInteractionSource() }
+                    val isRecordPressed by recordInteractionSource.collectIsPressedAsState()
+                    val playInteractionSource = remember { MutableInteractionSource() }
+                    val isPlayPressed by playInteractionSource.collectIsPressedAsState()
+
+                    var recordText by remember { mutableStateOf("Record audio") }
+                    var playText by remember { mutableStateOf("Play audio") }
+                    if (isRecordPressed) {
+                        // Pressed
+                        recordText = "Recording..."
+                        File(cacheDir, "audio.mp3").also {
+                            recorder.start(it)
+                            audioFile = it
+                        }
+
+                        DisposableEffect(Unit) {
+                            onDispose {
+                                // Released
+                                recorder.stop()
+                                recordText = "Record audio"
+                            }
+                        }
+                    }
+                    if (isPlayPressed) {
+                        // Pressed
+                        playText = "Playing..."
+                        player.playFile(audioFile ?: return@Column)
+
+                        DisposableEffect(Unit) {
+                            onDispose {
+                                // Released
+                                player.stop()
+                                playText = "Play audio"
+                            }
+                        }
+                    }
+                    Button(onClick = {}, interactionSource = recordInteractionSource) {
+                        Text(text = recordText)
+                    }
+                    Button(onClick = {}, interactionSource = playInteractionSource) {
+                        Text(text = playText)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SightsyncTheme {
-        Greeting("Android")
     }
 }
