5d747b12e921cbbb2ed5ef517682f0469aa7f65a
FerranAD <ferranaran@tuta.io>
2023-12-03 06:09:29 +0100

    fix

--- app/src/main/java/com/example/sightsync/api/other/api.kt
+++ app/src/main/java/com/example/sightsync/api/other/api.kt
@@ -1,14 +1,26 @@
 package com.example.sightsync.api.other
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiRetrofitClient {
     private const val BASE_URL = "https://k3bjexxnf0ndvt-8000.proxy.runpod.net/"
+    private const val CONNECT_TIMEOUT_SECONDS = 60
+    private const val READ_TIMEOUT_SECONDS = 60
+    private const val WRITE_TIMEOUT_SECONDS = 60
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(CONNECT_TIMEOUT_SECONDS.toLong(), TimeUnit.SECONDS)
+        .readTimeout(READ_TIMEOUT_SECONDS.toLong(), TimeUnit.SECONDS)
+        .writeTimeout(WRITE_TIMEOUT_SECONDS.toLong(), TimeUnit.SECONDS)
+        .build()
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(JacksonConverterFactory.create())
             .build()
     }
